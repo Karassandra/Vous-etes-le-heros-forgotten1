188da4c4b7204a447fcd7da9e2df20cadf9774b2
Karassandra <sararahmounpro@gmail.com>
2021-10-12 23:23:31 -0400

    sky

--- script.js
+++ script.js
@@ -1,51 +1,61 @@
-let chapitre1 = {
-  subtitle: "school",
-  text: "The moon is is glowing. It's your first time seeing it that shiny. You see a building in front of you. A school. You get shiver through your spine. What do you do ?",
-  img: "school_night.jpg",
-};
-
-let chapitre2 = {
-  subtitle: "picture1",
-  text: "You decide to enter the school. It's filled with darkness. Nobody's here. You stumbled upon a picture on the ground. You crouch to have a better look at it. It's a little bit torn. There's two people on the picture. You recognize your face. Besides you is..... a girl... You get chills. She looks familiar but you can't a grip of who she is. For some reason, you're happy and sad at the same time. What do you do ?",
-  img: "picture.jpg",
-};
-
-let chapitre3 = {
-  subtitle: "shadow",
-  text: "Right as you turn right you see a shadow at the end of the hallway. It's a person. O̵̦̙̎̊͌̈́̀̾̚ŕ̶͎̯̄͑̀ ̷̯͈̲̣̬͊̉͒͐i̸̼̖̎̀͗̒s̷͚̉͘͝ ̸̦͚͙̻͇͕̲́̀ȉ̴̧̺̞̳̮̲̌̚t̶̡̝͍̪͎̫̍̈ :) . What do you do ? ",
-  img: "shadow.jpg",
-};
-
-let chapitre3_1 = {
-  subtitle: "shadow",
-  text: "Right as you turn right you see a shadow at the end of the hallway. It's a person. O̵̦̙̎̊͌̈́̀̾̚ŕ̶͎̯̄͑̀ ̷̯͈̲̣̬͊̉͒͐i̸̼̖̎̀͗̒s̷͚̉͘͝ ̸̦͚͙̻͇͕̲́̀ȉ̴̧̺̞̳̮̲̌̚t̶̡̝͍̪͎̫̍̈ :) . What do you do ? ",
-  img: "shadow.jpg",
-};
-
-let chapitre4 = {
-  subtitle: "picture2",
-  text: "You decide to get close to it. It turns to face you. Huh ? It's a girl with long blond hair and beautiful blue eyes. As she see you, she shower you with a splendid smile. 'She looks cute.' You thought. 'You're late. I waited for you. Do you have the picture ?'",
-  img: "picture2.jpg",
-};
-
-let chapitre4_1 = {
-  subtitle: "picture2",
-  text: "You decide to get close to it. It turns to face you. Huh ? It's a girl with long blond hair and beautiful blue eyes. As she see you, she shower you with a splendid smile. 'She looks cute.' You thought. 'You're late. I waited for you. Do you have the picture ?'",
-  img: "picture2.jpg",
-};
-
 let chaptersObj = {
-  school: chapitre1,
-  picture1: chapitre2,
-  shadow: chapitre3,
-  shadow: chapitre3_1,
-  picture2: chapitre4,
-  picture2: chapitre4_1,
-};
+  waking_up: {
+    subtitle: "waking_up",
+    text: "You wake up not knowing where you are. The moon is glowing. The stars are shining. It’s your first time seeing it that bright… Wait… What’s your name again ?",
+    img: "sky.jpg",
+    option: [
+      {
+        text: "",
+        action: "",
+      },
+    ],
+  },
 
-function goToChapter(chapterName) {
-  let chapitre = chaptersObj;
-  console.log(chapitre);
-  console.log(chapitre.subtitle);
-  console.log(chapitre.text);
-}
+
+},
+
+// let chapitre2 = {
+//   subtitle: "picture1",
+//   text: "You decide to enter the school. It's filled with darkness. Nobody's here. You stumbled upon a picture on the ground. You crouch to have a better look at it. It's a little bit torn. There's two people on the picture. You recognize your face. Besides you is..... a girl... You get chills. She looks familiar but you can't a grip of who she is. For some reason, you're happy and sad at the same time. What do you do ?",
+//   img: "picture.jpg",
+// };
+
+// let chapitre3 = {
+//   subtitle: "shadow",
+//   text: "Right as you turn right you see a shadow at the end of the hallway. It's a person. O̵̦̙̎̊͌̈́̀̾̚ŕ̶͎̯̄͑̀ ̷̯͈̲̣̬͊̉͒͐i̸̼̖̎̀͗̒s̷͚̉͘͝ ̸̦͚͙̻͇͕̲́̀ȉ̴̧̺̞̳̮̲̌̚t̶̡̝͍̪͎̫̍̈ :) . What do you do ? ",
+//   img: "shadow.jpg",
+// };
+
+// let chapitre3_1 = {
+//   subtitle: "shadow",
+//   text: "Right as you turn right you see a shadow at the end of the hallway. It's a person. O̵̦̙̎̊͌̈́̀̾̚ŕ̶͎̯̄͑̀ ̷̯͈̲̣̬͊̉͒͐i̸̼̖̎̀͗̒s̷͚̉͘͝ ̸̦͚͙̻͇͕̲́̀ȉ̴̧̺̞̳̮̲̌̚t̶̡̝͍̪͎̫̍̈ :) . What do you do ? ",
+//   img: "shadow.jpg",
+// };
+
+// let chapitre4 = {
+//   subtitle: "picture2",
+//   text: "You decide to get close to it. It turns to face you. Huh ? It's a girl with long blond hair and beautiful blue eyes. As she see you, she shower you with a splendid smile. 'She looks cute.' You thought. 'You're late. I waited for you. Do you have the picture ?'",
+//   img: "picture2.jpg",
+// };
+
+// let chapitre4_1 = {
+//   subtitle: "picture2",
+//   text: "You decide to get close to it. It turns to face you. Huh ? It's a girl with long blond hair and beautiful blue eyes. As she see you, she shower you with a splendid smile. 'She looks cute.' You thought. 'You're late. I waited for you. Do you have the picture ?'",
+//   img: "picture2.jpg",
+// };
+
+// let chaptersObj = {
+//   waking_up: chapitre1,
+//   picture1: chapitre2,
+//   shadow: chapitre3,
+//   shadow: chapitre3_1,
+//   picture2: chapitre4,
+//   picture2: chapitre4_1,
+// };
+
+// function goToChapter(chapterName) {
+//   let chapitre = chaptersObj;
+//   console.log(chapitre);
+//   console.log(chapitre.subtitle);
+//   console.log(chapitre.text);
+// }
